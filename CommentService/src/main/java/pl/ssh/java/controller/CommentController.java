1f025588da5d6442b8957ce037ea6d25a66135ed
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2021-05-31 23:11:59 +0200

    Finished comment service

--- CommentService/src/main/java/pl/ssh/java/controller/CommentController.java
+++ CommentService/src/main/java/pl/ssh/java/controller/CommentController.java
@@ -1,6 +1,9 @@
 package pl.ssh.java.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 import pl.ssh.java.entity.Comment;
 import pl.ssh.java.entity.CommentDto;
 import pl.ssh.java.repository.CommentsRepository;
@@ -33,5 +36,33 @@ public class CommentController {
     @GetMapping("/{id}")
     public ArrayList<Comment> getByParentId(@ModelAttribute("id") UUID parentId){
         return commentsRepository.getCommentsByParentId(parentId);
+    }
+
+    @GetMapping("/{parentId}/{commentId}")
+    public Comment getComment(@ModelAttribute("parentId") UUID parentId, @ModelAttribute("commentId") UUID commentId){
+        var comment = commentsRepository.getComment(parentId, commentId);
+
+        if(comment == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Comment does not exists");
+        }
+
+        return comment;
+    }
+
+    @PostMapping("/{id}")
+    public void updateComment(@ModelAttribute("id") UUID commendId,  @RequestBody CommentDto commentDto){
+        var comment = commentsRepository.getComment(commentDto.parentId, commendId);
+        comment.setAuthor(commentDto.author);
+        comment.setContent(commentDto.content);
+        commentsRepository.update(comment);
+    }
+
+    @DeleteMapping("/{parentId}/{commentId}")
+    public void deleteComment(@ModelAttribute("parentId") UUID parentId, @ModelAttribute("commentId") UUID commendId){
+        var comment = commentsRepository.getComment(parentId, commendId);
+
+        if(comment != null){
+            commentsRepository.delete(comment);
+        }
     }
 }
